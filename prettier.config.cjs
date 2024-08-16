28e5bd4b5eb5bfc0c411587a624689a6ed454cd5
dekoms <dekoms@naver.com>
2024-08-17 00:35:55 +0900

    style: 필요없는 플러그인 제거

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -3,10 +3,10 @@ module.exports = {
   semi: true,
   singleQuote: true,
   jsxSingleQuote: false,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   bracketSpacing: true,
-  arrowParens: 'avoid',
+  arrowParens: "avoid",
 };
